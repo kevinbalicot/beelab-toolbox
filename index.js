24e855be68bc1abe0a08a4bdf732a152a9ad564c
kevin <kevinbalicot@gmail.com>
2020-10-14 09:35:54 +0200

    feat: Add server bootstrap

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 module.exports = {
     matcher: require('./src/services/matcher'),
     request: require('./src/services/request'),
+    bootstrapServer: require('./src/server/bootstrap'),
 
     Query: require('./src/services/query'),
     YoutubeSearchRepository: require('./src/repostories/youtube/search'),
---
+++ src/server/bootstrap.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const request = require('../services/request');
+
+module.exports = (app, options = { cache: true }) => {
+    const { NODE_ENV = 'dev', LOCALE = 'en', API_URL, ALLOW_ANONYMOUS, WEBSOCKET_URL } = process.env;
+    const basePath = process.cwd();
+    const cache = {
+        'Cache-Control': 'public, max-age=' + (86400 * 30),
+        'ETag': Date.now()
+    };
+
+    if (options.cache) {
+        // validate cache
+        app.use((req, res, next) => {
+            if (req.headers['if-none-match'] && req.headers['if-none-match'] === cache['ETag']) {
+                return res.status(304).send();
+            }
+
+            next();
+        });
+    }
+
+    app.link('/modules', basePath + '/node_modules', options.cache ? cache : {});
+    app.link('/dist', basePath + '/dist', options.cache ? cache : {});
+    app.link('/style', basePath + '/public/styles', options.cache ? cache : {});
+    app.link('/images', basePath + '/public/images', options.cache ? cache : {});
+
+    app.get('/info', (req, res) => {
+        const { version, name } = require(`${basePath}/package.json`);
+        const config = { locale: LOCALE, env: NODE_ENV, allowAnonymous: !!ALLOW_ANONYMOUS };
+
+        if (API_URL) {
+            config['api'] = API_URL;
+        }
+
+        if (WEBSOCKET_URL) {
+            config['websocketUrl'] = WEBSOCKET_URL;
+        }
+
+        request(`https://back.beelab.tk/configuration/${name}`, { headers: { 'User-Agent': req.userAgent } })
+            .then(data => res.json(Object.assign(config, { version, name }, JSON.parse(data))));
+    });
+
+    app.get('/', (req, res) => {
+        const path = basePath + '/public/index.html';
+        try {
+            fs.statSync(path);
+            res.sendFile(path, 'index.html', 'text/html', false);
+        } catch (e) {
+            res.status(404).send(e);
+        }
+    });
+};
